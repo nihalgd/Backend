7641f47f7e403468607a656a4f6e8beac38b52ed
nihalgd <nihalgoud.12345@gmail.com>
2026-04-03 23:41:08 +0530

    Learn Back-end

--- 4-node-ejs/index.js
+++ 4-node-ejs/index.js
@@ -9,7 +9,15 @@ app.set('view engine' , 'ejs');
 
 app.get("/" , function(req , res){
     res.render("index")
-})
+});
+
+app.get("/profile/:username" , function(req , res){
+    res.send(` welcome , ${req.params.username}`)
+});
+
+app.get("/author/:username/:age" , function(req , res){
+    res.send(` welcome , ${req.params.username} of age ${req.params.age} `)
+});
 
 app.listen(3000 , function (){
     console.log("its fuckingg runnning!");
