ae25e300bb0fecabf787f37baf575bfe7ea3e5ee
nihalgd <nihalgoud.12345@gmail.com>
2026-04-08 00:52:48 +0530

    Learn Back-end

--- 7-mongo-db/app.js
+++ 7-mongo-db/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express();
 const path = require('path');
+const usermodel = require('./models/user');
+const { read } = require('fs');
 
 
 app.set('view engine' , 'ejs');
@@ -11,6 +13,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/" , function(req , res){
     res.render("index")
+})
+
+app.get("/read", async function (req, res) {
+    let allUsers = await usermodel.find(); 
+    res.render("read", { users: allUsers });
+});
+
+app.get("/delete/:id", async function (req, res) {
+    let users = await usermodel.findOneAndDelete({_id: req.params.id}); 
+    res.redirect("/read")
+});
+
+app.post("/create" , async function(req,res){
+   let {name , email , image} = req.body;
+
+   let createdUser = await usermodel.create({
+     name,
+     email,
+     image
+   });
+
+   res.redirect("./read")
 })
 
 app.listen(3000);
