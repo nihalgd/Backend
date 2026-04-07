9c16fe341b915f1e8436bb01099cb73ab5078841
nihalgd <nihalgoud.12345@gmail.com>
2026-04-07 17:17:11 +0530

    Learn back-end

--- 6-mongo-db/app.js
+++ 6-mongo-db/app.js
@@ -10,15 +10,26 @@ app.get("/" , function(req,res){
 
 app.get("/create" , async function(req,res){
     let createuser = await usermodel.create({
-        name : "Nihal",
-        email : "nihalgoud.12345@gmail.com",
+        name : "Niharika",
+        email : "nihalgoud@gmail.com",
         username : "nihal_04"
     })
     res.send(createuser);
 })
 
-app.get("/update" , async function(req,res){
-   
+app.get("/read" , async function(req,res){
+    let users = await usermodel.find({username : "nihal_04"})
+    res.send(users)
+})
+
+app.get("/updated" , async function(req,res){
+    let updateuser = await usermodel.findOneAndUpdate({name:"Nihal"} , {name : "Nihal Goud"} , {new : true})
+    res.send(updateuser);
+})
+
+app.get("/delete" , async function(req,res){
+    let deleteUser = await usermodel.findOneAndDelete({name : "Nihal Goud"})
+    res.send(deleteUser)
 })
 
 app.listen(3000);
